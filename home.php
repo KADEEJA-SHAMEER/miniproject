7558a8140c0767d3f39e309cd19206745a585d7d
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-08-12 13:50:18 +0530

    first

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="homestyle.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="div3">
+    <div class="div1">
+        <img src="./logo.svg" width=20%></div>
+    <div >
+        <form action="login.php" method="post">
+            <button type=submit name=login >LOGIN </button>
+       </form>
+</div> 
+</div>
+<div class="txt1">
+    <h1> WHERE WORKS<BR>MEET FREEDOM</h1>
+</div>
+</body>
+</html>
