a35ca39c7c9920e6314ca590c4d7eeb2a775310c
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-08-14 11:23:16 +0530

    updated

--- register.php
+++ register.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script>
+
+        const form = document
+
+    </script>
 </head>
 <body>
-    <form method="post">
+    <form action="register.php" onsubmit="return validate()" method="post">
       <input type="text"  name="name" placeholder="enter your name" required><br>
       <input type="email" name="email" placeholder="enter your email" required><br>
       <input type="password" name="pword"  placeholder="enter password" required><br>
@@ -27,20 +32,16 @@ if(isset($_POST['REGISTER']))
     $pword=$_POST['pword'];
     $cpword=$_POST['cpword'];
     $role=$_POST['role'];
-    echo "$name";
-    echo"$pword";
-    echo"$cpword";
-    if($pword==$cpword)
+    if($pword == $cpword)
     {
-        echo "<script><alert>password doesn't match re enter</alert></script>";
         $sql="INSERT INTO `users`(`Name`, `email`, `password`, `role`) VALUES 
         ('$name','$email','$pword','$role')";
-        $data=mysqli_query($dbcon,$sql);
+        $data=mysqli_query($conn,$sql);
         if(!$data)
         {
                 echo "error inserting values ";
         }
-        header(location:'./login.php');
+        header("Location: /miniproject/login.php");
     }
     else{
         echo "<script><alert>password doesn't match re enter</alert></script>";
