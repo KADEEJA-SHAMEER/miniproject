a91b888335394c28dcc8a56d1cc439b7d95f4f81
KADEEJA-SHAMEER <kadeejashameer110@gmail.com>
2024-08-21 12:40:37 +0530

    updated

--- connect.php
+++ connect.php
@@ -26,10 +26,10 @@ if($conn->query($sql)===FALSE){
 }
 
 $sql="CREATE TABLE IF NOT EXISTS job_provider(
-    user_id INT(5) not null,
-    job_provider_id INT(5) PRIMARY KEY AUTO_INCREMENT,
+    user_id INT(5) not  null,
     company_name VARCHAR(30) NOT NULL,
     phone_no int(10) Not null,
+    industry varchar(20) not null,
     FOREIGN KEY(user_id) REFERENCES users(user_id))";
     if($conn->query($sql)===FALSE){
         die("error creating table: ".$conn->error);
@@ -37,7 +37,6 @@ $sql="CREATE TABLE IF NOT EXISTS job_provider(
 
     $sql="CREATE TABLE IF NOT EXISTS job_seeker(
         user_id INT(5) not null,
-        job_seeker_id INT(5) PRIMARY KEY AUTO_INCREMENT,
         skills VARCHAR(30) NOT NULL,
         education ENUM('High School', 'Diploma', 'Bachelor\'s Degree', ' Master\'s Degree', 'PhD', 'Other') Not null,
         FOREIGN KEY(user_id) REFERENCES users(user_id))";
--- home.php
+++ home.php
@@ -15,6 +15,5 @@
        <a href="login.php" class="button">LOGIN</a>
     </div>
     <h1 class="title"> WHERE <BR>   WORKS MEET <BR>FREEDOM</h1>
-    sdjflsd
 </body>
 </html>
