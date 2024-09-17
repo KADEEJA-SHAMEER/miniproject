af2a838f989b773dde5e0e428faa51d9f44cbc35
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-09-16 19:56:09 -0700

    updated

---
+++ dashboard-jobpost.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style-dashboard.css">
+</head>
+<body>
+<div class="header">
+       <h1>
+        <a href="home.php">  <img src="./logo.svg" width=20%></a>
+       </h1>
+       <a href="register.html"><img src="./contact_9921248.png" width=10%></a>
+    </div>
+</body>
+</html>
