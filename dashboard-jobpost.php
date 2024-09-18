315cd2d6e0f038ec0ab3cdff3afa98e8a8404501
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-09-18 13:51:49 -0700

    updated

--- dashboard-jobpost.php
+++ dashboard-jobpost.php
@@ -4,22 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style-dashboard.css">
 </head>
-<body>
-<div class=header>
-    <div class=logo>
-      <a href="home.php">  <img src="./logo.svg" ></a>
-    </div>
-    <div class=profile>
-      <a href="job-provider.html">  <img src="./contact_9921248.png"></a>
-    </div>
-</div>
-<div class="sidebar">
-  <a href="job-posting.html" target=frame2>POST JOB</a><br>
-  <a href=job-posting.html>VIEW POSTED JOB</a><br>
-  <a href=job-posting.html>REQUESTS</a><br>
-  <a href=job-posting.html>LOG OUT</a><br>
-</div>
-</body>
+<frameset rows="12%,*" border="none">
+  <frame src="dashboardheader.html" >
+    <frameset cols="15%,*">
+      <frame src="sidebar.html" >
+      <frame src="main.html" name="frame2">
+</frameset>
+</frameset>
 </html>
