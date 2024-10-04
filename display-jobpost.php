1ba03dfa6b44be97bd10faa93279026fb7fbbf0f
Kadeeja-kadu <kadeejashameer110@gmail.com>
2024-10-03 21:51:52 -0700

    updated

--- display-jobpost.php
+++ display-jobpost.php
@@ -56,28 +56,47 @@
      while($row=mysqli_fetch_array($data))
      {
         echo"<form action='' method='post'>";
+        echo "<input type='hidden' name='post_id' value=".$row['job_post_id'].">";
         echo"<tr>";
-        echo "<td><input type='text' name='job_title' value=".$row['job_title']."></td>";
+        echo "<td><input type='text' name='jobtitle' value=".$row['job_title']."></td>";
         echo "<td><textarea name='schedule_req' rows=3 cols=50 >".$row['schedule_requirement']."</textarea></td>";
         echo "<td><textarea name='location' rows=5 cols='50'>".$row['location']."</textarea></td>";
         echo "<td> <textarea name='description' rows=5 cols=50 >".$row['description']."</textarea></td>";
         echo "<td><input type='date' name='post_date' value=".$row['posted_date']."></td>";
         echo "<td> <input type='number' name='salary' value=".$row['salary']."></td>";
-        echo "<td><button type=submit name=update>UPDATE</button>";
-        echo "<td><button type=submit name=delete>DELETE</button>";
+        echo "<td><button type=submit name=update>UPDATE</button></td>";
+        echo "<td><button type=submit name=delete>DELETE</button></td>";
         echo "</tr>";
+        echo "</form>";
     }
     echo "</table>";
    }
-   /*if(isset($_POST['update']))
+   if(isset($_POST['update']))
    {
-    $job_title=$_POST['job_title'];
+    $post_id=$_POST['post_id'];
+    $job_title=$_POST['jobtitle'];
     $schedule_req=$_POST['schedule_req'];
     $location=$_POST['location'];
     $description=$_POST['description'];
     $post_date=$_POST['post_date'];
     $salary=$_POST['salary'];
-   }*/
+    $sql="UPDATE `job_posting` SET `job_title`='$job_title',
+    `schedule_requirement`='$schedule_req',`location`='$location',`description`='$description',
+    `posted_date`='$post_date',`salary`='$salary' WHERE `job_post_id`='$post_id'";
+     if($conn->query($sql)===FALSE){
+      die("error updating value: ".$conn->error);
+  }
+   }
+   if(isset($_POST['delete']))
+   {
+    $post_id=$_POST['post_id'];
+    $sql="DELETE FROM `job_posting` WHERE `job_post_id`='$post_id' ";
+    if($conn->query($sql)===FALSE){
+      die("error updating value: ".$conn->error);
+  }else{
+    echo "<script>alert('POST DELETED SUCCESSFULLY')</script>";
+  }
+   }
    ?>
 </body>
 </html>
